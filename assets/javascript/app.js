4dacbd74a33b2cc95bf4427ba59e68ec0dc7ec73
Jared Black <jaredblackjcb@gmail.com>
2023-01-09 20:43:59 -0600

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+import * as JsCookie from "js-cookie"; // generated
+
+export { Payments } from './payments';
+export { Modals } from './web/modals';
+
+// pass-through for Cookies API
+export const Cookies = JsCookie.default;
