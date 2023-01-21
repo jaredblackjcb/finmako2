1f00e3889b904ca10367c55d47cec8c6160178cd
Jared Black <jaredblackjcb@gmail.com>
2023-01-20 19:24:56 -0600

    Remove pegasus example code

--- assets/javascript/api-client/models/index.ts
+++ assets/javascript/api-client/models/index.ts
@@ -1,11 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './AggregateEmployeeData';
 export * from './CreateCheckout';
-export * from './DepartmentEnum';
-export * from './Employee';
-export * from './PaginatedEmployeeList';
-export * from './PatchedEmployee';
 export * from './Price';
 export * from './Product';
 export * from './ProductMetadata';
